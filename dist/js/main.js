76c1eb7103bb88d6139f3f3cefd013d4490698f5
Pavel Proyavin <pavel@dorohovdesign.com>
2019-11-27 11:15:35 +0300

    init

---
+++ dist/js/main.js
@@ -0,0 +1,16 @@
+(function($) {
+    "use strict"
+    $(function() {
+
+        $('.__dd').mousemove(function(e) {
+            $('.__dd__block-fo__part.is--one').css({
+                transform: 'translateX(' + e.clientX / 30 + 'px) translateY(' + e.clientY / 30 + 'px)'
+            })
+            $('.__dd__block-fo__part.is--two').css({
+                transform: 'translateX(-' + e.clientX / 30 + 'px) translateY(-' + e.clientY / 30 + 'px)'
+            }) 
+        })
+
+    })
+})(jQuery);
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24oJCkge1xyXG4gICAgXCJ1c2Ugc3RyaWN0XCJcclxuICAgICQoZnVuY3Rpb24oKSB7XHJcblxyXG4gICAgICAgICQoJy5fX2RkJykubW91c2Vtb3ZlKGZ1bmN0aW9uKGUpIHtcclxuICAgICAgICAgICAgJCgnLl9fZGRfX2Jsb2NrLWZvX19wYXJ0LmlzLS1vbmUnKS5jc3Moe1xyXG4gICAgICAgICAgICAgICAgdHJhbnNmb3JtOiAndHJhbnNsYXRlWCgnICsgZS5jbGllbnRYIC8gMzAgKyAncHgpIHRyYW5zbGF0ZVkoJyArIGUuY2xpZW50WSAvIDMwICsgJ3B4KSdcclxuICAgICAgICAgICAgfSlcclxuICAgICAgICAgICAgJCgnLl9fZGRfX2Jsb2NrLWZvX19wYXJ0LmlzLS10d28nKS5jc3Moe1xyXG4gICAgICAgICAgICAgICAgdHJhbnNmb3JtOiAndHJhbnNsYXRlWCgtJyArIGUuY2xpZW50WCAvIDMwICsgJ3B4KSB0cmFuc2xhdGVZKC0nICsgZS5jbGllbnRZIC8gMzAgKyAncHgpJ1xyXG4gICAgICAgICAgICB9KSBcclxuICAgICAgICB9KVxyXG5cclxuICAgIH0pXHJcbn0pKGpRdWVyeSk7Il19
