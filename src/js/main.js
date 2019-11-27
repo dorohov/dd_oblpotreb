74fc192204d3299bde7900bf4114609c8523c694
Pavel Proyavin <pavel@dorohovdesign.com>
2019-11-27 17:40:15 +0300

    init

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,41 @@
     "use strict"
     $(function() {
 
-        
+        $('.__dd').mousemove(function(e) {
+            $('[data-tr]').css({
+                transform: 'translateX(' + e.clientX / 60 + 'px) translateY(' + e.clientY / 60 + 'px)'
+            })
+            $('[data-tri]').css({
+                transform: 'translateX(-' + e.clientX / 60 + 'px) translateY(-' + e.clientY / 60 + 'px)'
+            }) 
+        })
+
+        function startAnimationBlocks() {
+            var blockItems = $('.is--animate-block')
+            blockItems.each(function() {
+                var thisBlock = $(this)
+                var thisAnim = $(this).data('animname')
+                var thisDelay = $(this).data('animdelay')
+
+                if($(this).visible(true) && !$(this).hasClass('is--finish')) {
+                    thisBlock
+                        .addClass('animated')
+                        .addClass(thisAnim)
+                        .css({
+                            "-webkit-animation-delay": thisDelay + 's',
+                            "-moz-animation-delay": thisDelay + 's',
+                            "-o-animation-delay": thisDelay + 's',
+                            "animation-delay": thisDelay + 's'
+                        })
+                }
+            })
+        }
+
+        startAnimationBlocks()
+
+        $(document).scroll(function() {
+            startAnimationBlocks()
+        })
 
     })
 })(jQuery);
